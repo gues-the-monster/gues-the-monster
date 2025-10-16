d054a4c0706d756362490e77f417067983ffd641
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2025-10-15 22:10:50 -0300

    refactor: migrated main project files to TS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='bg-gray-700 w-full h-screen flex items-center justify-center'>
+        <h1 className='text-white text-4xl'>Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;

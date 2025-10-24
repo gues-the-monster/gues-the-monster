731c2b4e425e405402a0877f005c220c54a2bb6c
Juan V. Pino Contreras <juan.vpinoc@gmail.com>
2025-10-24 13:53:07 -0300

    refactor: title and message position/"siguiente monstruo" button position fixed and bounce animation

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -24,24 +24,25 @@ export default function Game() {
   }
 
   return (
-    <div className='container text-center'>
-      <h1 className='text-4xl md:text-5xl mb-4 text-red-700 text-shadow-lg/30'>
+    <div className='container relative'>
+      <h1 className='text-5xl text-center md:text-start md:text-6xl md:ms-7 mb-2 text-red-700 text-shadow-lg/30'>
         Guess The Monster{' '}
       </h1>
       {monster && (
         <>
-          <MonsterView estado={estado} imagen={imagen} />
           <Message texto={mensaje} estado={estado} />
+          <MonsterView estado={estado} imagen={imagen} />
+
           <Options
             opciones={monster.opciones}
             onSelect={() => null} // Manejar la selección de opciones
-            disabled={estado !== 'silueta'}
+            disabled={estado !== 'silueta'} // Deshabilitar opciones si no está en estado 'silueta'
           />
 
           {estado !== 'silueta' && (
             <button
               onClick={cargarMonstruo}
-              className='mt-4 px-4 py-2 bg-purple-700 text-white rounded-lg'
+              className='fixed bottom-5 right-0 left-0 px-4 py-2 bg-gray-700 text-2xl text-white hover:bg-gray-900 transition-colors duration-300 animate-bounce'
             >
               Siguiente monstruo 👻
             </button>
